37e44440f58d8165d2a62be332d491969f68a322
ronkips <kipshillash@gmail.com>
2023-01-19 13:29:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 function App() {
+  let [data1, setData1] = useState([]);
+  let [firstPage, setFirstPage] = useState(null);
+  let [finalPage, setFinalPage] = useState(null);
+  let initialPage = 0;
+  let lastPage = 0;
+  let allData = [];
+  const numberOfPages = async () => {
+    try {
+      let countPages = await axios({
+        method: "GET",
+        url: `https://app.geckoterminal.com/api/p1/eth/pools?include=dex,dex.network,dex.network.network_metric,tokens&page=1}&include_network_metrics=true`,
+        // headers: {
+        //   "Content-Type": "application/json"
+        // }
+      }).then(function (response) {
+        return response.data.links.last.meta.series;
+      });
+      initialPage = countPages[0];
+      lastPage = countPages[4];
+    } catch (error) {
+      console.log("errors", error);
+    }
+    setFirstPage(initialPage);
+    setFinalPage(lastPage);
+    endP();
+  };
+  const endP = async () => {
+    try {
+      console.log("inside final", finalPage);
+      for (let page = firstPage; page <= 3; page++) {
+        let newArr = await axios({
+          method: "GET",
+          url: `https://app.geckoterminal.com/api/p1/eth/pools?include=dex,dex.network,dex.network.network_metric,tokens&page=${page}&include_network_metrics=true`,
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(function (response) {
+          console.log("res", response.data.data);
+          return response.data.data;
+        });
+        allData.push(...newArr);
+      }
+      setData1(allData);
+    } catch (error) {
+      console.log("err", error);
+    }
+  };
+  useEffect(() => {
+    numberOfPages();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table style={{ width: "50%" }}>
+        <thead>
+          <th>Number</th>
+          <th>Address</th>
+          <th>Name</th>
+          <th>relation id</th>
+        </thead>
+        <tbody>
+          {data1.map(({ attributes, relationships }, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{attributes.address}</td>
+                <td>{attributes.name}</td>
+                <td>
+                  {relationships.tokens.data.map(({ id }) => {
+                    return `, ${id}`;
+                  })}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
